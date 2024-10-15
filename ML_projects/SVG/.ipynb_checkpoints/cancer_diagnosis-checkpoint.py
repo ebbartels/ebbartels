5e8523634d4da45dcda323285dbac09d1922b600
ebbartels <emmettbartels@gmail.com>
2024-10-15 17:12:44 -0500

    pushing SVG and start of MNIST

---
+++ ML_projects/SVG/.ipynb_checkpoints/cancer_diagnosis-checkpoint.py
@@ -0,0 +1,41 @@
+# +
+from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, classification_report
+
+
+
+# +
+from ucimlrepo import fetch_ucirepo 
+  
+# fetch dataset 
+breast_cancer_wisconsin_diagnostic = fetch_ucirepo(id=17) 
+  
+# data (as pandas dataframes) 
+X = breast_cancer_wisconsin_diagnostic.data.features 
+y = breast_cancer_wisconsin_diagnostic.data.targets 
+  
+# metadata 
+#print(breast_cancer_wisconsin_diagnostic.metadata) 
+  
+# variable information 
+print(breast_cancer_wisconsin_diagnostic.variables) 
+# -
+
+X
+
+y
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=20)
+
+model = SVC(kernel="linear", C=1.0)
+
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+accuracy_score(y_test, y_pred)
+
+print(classification_report(y_test, y_pred))
+
+
